31da73a89ecffc84bc4945711703b9876e3849b5
Carla <karlla.moraes960@gmail.com>
2025-04-30 15:23:31 -0300

    interpolate mean musa

--- R/interpolate_mean_musa.R
+++ R/interpolate_mean_musa.R
@@ -1,37 +1,31 @@
 #' Interpolate Accumulated Mean Values at an Unobserved Location
 #'
 #' This function interpolates the accumulated mean values for a location where no data was observed,
-#' using outputs from the STModelWeibullMCMC model. The interpolation is performed over a vector of times.
+#' using outputs from the STModelMusaOkumotoMCMC model. The interpolation is performed over a vector of times.
 #'
-#' @param results A list containing the output from `STModelWeibullMCMC`, including:
+#' @param results A list containing the output from `STModelMusaOkumotoMCMC`, including:
 #'   - `MW`: Samples for parameter W.
-#'   - `MMj`: Samples for parameter M.
-#'   - `Mvw`: Samples for variance sigma^2_w.
-#'   - `Mvm`: Samples for variance sigma^2_m.
-#'   - `Mbw`: Samples for spatial decay phi_w.
-#'   - `Mbm`: Samples for spatial decay phi_m.
-#'   - `MPsi`: Samples for regression coefficients Psi.
-#'   - `MBeta`: Samples for regression coefficients Beta.
+#'   - `Malpha`: Samples for parameter alpha.
+#'   - `Mb`: Samples for parameter phi.
+#'   - `Mv`: Samples for parameter sigma^2.
+#'   - `MPsi`: Samples for parameter Psi.
 #' @param data A matrix of occurrence times for the event of interest. Each column corresponds to a monitoring station.
 #' @param sites A matrix of geographic coordinates where the process was observed.
+#' @param Xw Covariates associated with the scale parameter at the location.
 #' @param Sites1 A vector representing the geographic coordinates of the location where interpolation is to be performed.
 #' @param gridt A vector of times at which the accumulated mean is to be estimated.
-#' @param Xmr Covariates associated with the shape parameter at the location.
-#' @param Xwr Covariates associated with the scale parameter at the location.
-#' @param Xw aaaa
-#' @param Xm aaaaaa
+#' @param Xwr Covariates associated with the scale parameter at the location where interpolation is to be performed.
+#'
 #'
 #' @return A matrix (`MatMean`) containing the interpolated accumulated mean values at the times specified in `gridt`.
 #'
 #' @export
-interpolate_mean <- function(results, data, sites,Xm,Xw, Sites1, gridt, Xmr, Xwr) {
+interpolate_mean_musa<- function(results, data, sites,Xw, Sites1, gridt, Xwr) {
 
   data<-as.matrix(data)
   sites<-as.matrix(sites)
   Sites1<-as.matrix(Sites1)
-  Xm<-as.matrix(Xm)
   Xw<-as.matrix(Xw)
-  Xmr<-as.matrix(Xmr)
   Xwr<-as.matrix(Xwr)
 
   Stotal <- rbind(sites, t(as.matrix(Sites1)))
@@ -39,7 +33,7 @@ interpolate_mean <- function(results, data, sites,Xm,Xw, Sites1, gridt, Xmr, Xwr
   n <- nrow(sites)
 
   for (i in 1:length(results$MW[, 1])) {
-    SIGMAWtotal <- gSigma(results$Mbw[i], results$Mvw[i], Stotal)
+    SIGMAWtotal <- gSigma(results$Mb[i], results$Mv[i], Stotal)
     SIGMAWA12 <- t(as.matrix(SIGMAWtotal[1:n, (n + 1)]))
     SIGMAWA1 <- SIGMAWtotal[1:n, 1:n]
     A2estw <- t(as.matrix(Xwr)) %*% as.matrix(results$MPsi[i, ]) +
@@ -47,21 +41,13 @@ interpolate_mean <- function(results, data, sites,Xm,Xw, Sites1, gridt, Xmr, Xwr
     SIGMAA2estw <- SIGMAWtotal[(n + 1), (n + 1)] - SIGMAWA12 %*% solve(SIGMAWA1) %*% t(SIGMAWA12)
     WNO <- rnorm(1, A2estw, sd = sqrt(SIGMAA2estw))
 
-    Gama <- exp(WNO)
+    Gama <- WNO
 
-    SIGMAMtotal <- gSigma(results$Mbm[i], results$Mvm[i], Stotal)
-    SIGMAMA12 <- t(as.matrix(SIGMAMtotal[1:n, (n + 1)]))
-    SIGMAMA1 <- SIGMAMtotal[1:n, 1:n]
-    A2estm <- t(as.matrix(Xmr)) %*% as.matrix(results$MBeta[i, ]) +
-      SIGMAMA12 %*% solve(SIGMAMA1) %*% (as.matrix(results$MMj[i, ]) - Xm %*% as.matrix(results$MBeta[i, ]))
-    SIGMAA2estm <- SIGMAMtotal[(n + 1), (n + 1)] - SIGMAMA12 %*% solve(SIGMAMA1) %*% t(SIGMAMA12)
-    MNO <- rnorm(1, A2estm, sd = sqrt(SIGMAA2estm))
-
-    Eta <- exp(MNO)
+    Alpha<-results$Malpha[i]
 
     vect <- NULL
     for (j in 1:length(gridt)) {
-      Mean <- MeanFunction(Eta, Gama, gridt[j])
+      Mean <- mfMUSA(Gama,Alpha,gridt[j])
       vect <- c(vect, Mean)
     }
 
---
+++ R/interpolate_mean_weibull.R
@@ -0,0 +1,72 @@
+#' Interpolate Accumulated Mean Values at an Unobserved Location
+#'
+#' This function interpolates the accumulated mean values for a location where no data was observed,
+#' using outputs from the STModelWeibullMCMC model. The interpolation is performed over a vector of times.
+#'
+#' @param results A list containing the output from `STModelWeibullMCMC`, including:
+#'   - `MW`: Samples for parameter W.
+#'   - `MMj`: Samples for parameter M.
+#'   - `Mvw`: Samples for variance sigma^2_w.
+#'   - `Mvm`: Samples for variance sigma^2_m.
+#'   - `Mbw`: Samples for spatial decay phi_w.
+#'   - `Mbm`: Samples for spatial decay phi_m.
+#'   - `MPsi`: Samples for regression coefficients Psi.
+#'   - `MBeta`: Samples for regression coefficients Beta.
+#' @param data A matrix of occurrence times for the event of interest. Each column corresponds to a monitoring station.
+#' @param sites A matrix of geographic coordinates where the process was observed.
+#' @param Xw Covariates associated with the scale parameter at the location.
+#' @param Xm Covariates associated with the shape parameter at the location.
+#' @param Sites1 A vector representing the geographic coordinates of the location where interpolation is to be performed.
+#' @param gridt A vector of times at which the accumulated mean is to be estimated.
+#' @param Xwr Covariates associated with the scale parameter at the location where interpolation is to be performed.
+#' @param Xmr Covariates associated with the shape parameter at the location where interpolation is to be performed.
+#'
+#' @return A matrix (`MatMean`) containing the interpolated accumulated mean values at the times specified in `gridt`.
+#'
+#' @export
+interpolate_mean_weibull<- function(results, data, sites,Xw,Xm, Sites1, gridt, Xwr,Xmr) {
+
+  data<-as.matrix(data)
+  sites<-as.matrix(sites)
+  Sites1<-as.matrix(Sites1)
+  Xm<-as.matrix(Xm)
+  Xw<-as.matrix(Xw)
+  Xmr<-as.matrix(Xmr)
+  Xwr<-as.matrix(Xwr)
+
+  Stotal <- rbind(sites, t(as.matrix(Sites1)))
+  MatMean <- NULL
+  n <- nrow(sites)
+
+  for (i in 1:length(results$MW[, 1])) {
+    SIGMAWtotal <- gSigma(results$Mbw[i], results$Mvw[i], Stotal)
+    SIGMAWA12 <- t(as.matrix(SIGMAWtotal[1:n, (n + 1)]))
+    SIGMAWA1 <- SIGMAWtotal[1:n, 1:n]
+    A2estw <- t(as.matrix(Xwr)) %*% as.matrix(results$MPsi[i, ]) +
+      SIGMAWA12 %*% solve(SIGMAWA1) %*% (as.matrix(results$MW[i, ]) - Xw %*% as.matrix(results$MPsi[i, ]))
+    SIGMAA2estw <- SIGMAWtotal[(n + 1), (n + 1)] - SIGMAWA12 %*% solve(SIGMAWA1) %*% t(SIGMAWA12)
+    WNO <- rnorm(1, A2estw, sd = sqrt(SIGMAA2estw))
+
+    Gama <- exp(WNO)
+
+    SIGMAMtotal <- gSigma(results$Mbm[i], results$Mvm[i], Stotal)
+    SIGMAMA12 <- t(as.matrix(SIGMAMtotal[1:n, (n + 1)]))
+    SIGMAMA1 <- SIGMAMtotal[1:n, 1:n]
+    A2estm <- t(as.matrix(Xmr)) %*% as.matrix(results$MBeta[i, ]) +
+      SIGMAMA12 %*% solve(SIGMAMA1) %*% (as.matrix(results$MMj[i, ]) - Xm %*% as.matrix(results$MBeta[i, ]))
+    SIGMAA2estm <- SIGMAMtotal[(n + 1), (n + 1)] - SIGMAMA12 %*% solve(SIGMAMA1) %*% t(SIGMAMA12)
+    MNO <- rnorm(1, A2estm, sd = sqrt(SIGMAA2estm))
+
+    Eta <- exp(MNO)
+
+    vect <- NULL
+    for (j in 1:length(gridt)) {
+      Mean <- MeanFunction(Eta, Gama, gridt[j])
+      vect <- c(vect, Mean)
+    }
+
+    MatMean <- rbind(MatMean, t(as.matrix(vect)))
+  }
+
+  return(MatMean)
+}
